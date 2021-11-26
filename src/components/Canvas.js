94e136d6e795a8f06d5b1a9e833a2261a482f1d3
Jennifer Law <jenniferlaw@Jennifers-MacBook-Air-2.local>
2021-11-26 00:56:28 -0800

    console.log clean up

--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -167,9 +167,7 @@ const Canvas = (props) => {
     setElements(elementsCopy);
   };
 
-  console.log(selectedElement?.id);
   const handleMouseDown = (e) => {
-    console.log(elements);
     const { offsetX, offsetY } = e.nativeEvent;
     if (elementType === "select") {
       const element = getElementAtPosition(offsetX, offsetY, elements);
